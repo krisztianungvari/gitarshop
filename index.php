4a6e3698a35704efb271edba44e16958d9001c8e
Ungvári Krisztián <krisz270@gmail.com>
2022-02-28 10:11:56 +0100

    1.0 2022-02-28

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
+-->
+<html lang="hu">
+    <head>
+        <title>Gitarshop</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="icon" type="image/png" href="favicon-32x32.png">
+    </head>
+    <body>
+        <header>
+             <h1>Krisztián Ungvári Projektmunkája</h1>
+             <form action="#">
+                 <label>text</label>
+                 <input type="text">
+                 <label>text</label>
+                 <input type="password">
+             </form>
+        </header>
+       
+        <div id="content">
+            <nav>
+                <ul> 
+                <li> <a href="index.php?menu=fooldal">Főoldal</a> </li> 
+                <li> <a href="index.php?menu=gitarok">Gitárok</a> </li>
+                <li> <a href="index.php?menu=info">Infó</a> </li>
+                <li> <a href="index.php?menu=kapcsolat">Kapcsolat</a> </li>
+                </ul>
+                </nav>
+            <h2>Köszöntelek az oldalamon!</h2>
+            
+            
+              <nav>  
+           
+        </nav>
+            
+            
+        <img src="Slashborito.jpg" alt="Slashborito.jpg" title="Slash"/>
+        </div>
+        <footer>
+            &COPY; 2022
+        </footer>
+    </body>
+</html>
