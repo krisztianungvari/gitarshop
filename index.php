a66ff5c00bbdc1ca44bba3d7f45bffe43370e710
Ungvári Krisztián <krisz270@gmail.com>
2022-03-02 10:39:43 +0100

    1.1 2022-03-02

--- index.php
+++ index.php
@@ -1,48 +1,241 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
--->
+﻿<!DOCTYPE html>
 <html lang="hu">
-    <head>
-        <title>Gitarshop</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="style.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <link rel="icon" type="image/png" href="favicon-32x32.png">
-    </head>
-    <body>
-        <header>
-             <h1>Krisztián Ungvári Projektmunkája</h1>
-             <form action="#">
-                 <label>text</label>
-                 <input type="text">
-                 <label>text</label>
-                 <input type="password">
-             </form>
-        </header>
-       
-        <div id="content">
-            <nav>
-                <ul> 
-                <li> <a href="index.php?menu=fooldal">Főoldal</a> </li> 
-                <li> <a href="index.php?menu=gitarok">Gitárok</a> </li>
-                <li> <a href="index.php?menu=info">Infó</a> </li>
-                <li> <a href="index.php?menu=kapcsolat">Kapcsolat</a> </li>
-                </ul>
-                </nav>
-            <h2>Köszöntelek az oldalamon!</h2>
+  <head>
+    <meta charset="utf-8">
+    <title>Gitarshop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+<!--Less styles -->
+   <!-- Other Less css file //different less files has different color scheam
+	<link rel="stylesheet/less" type="text/css" href="themes/less/simplex.less">
+	<link rel="stylesheet/less" type="text/css" href="themes/less/classified.less">
+	<link rel="stylesheet/less" type="text/css" href="themes/less/amelia.less">  MOVE DOWN TO activate
+	-->
+	<!--<link rel="stylesheet/less" type="text/css" href="themes/less/bootshop.less">
+	<script src="themes/js/less.js" type="text/javascript"></script> -->
+	
+<!-- Bootstrap style --> 
+    <link id="callCss" rel="stylesheet" href="themes/bootshop/bootstrap.min.css" media="screen"/>
+    <link href="themes/css/base.css" rel="stylesheet" media="screen"/>
+<!-- Bootstrap style responsive -->	
+	<link href="themes/css/bootstrap-responsive.min.css" rel="stylesheet"/>
+	<link href="themes/css/font-awesome.css" rel="stylesheet" type="text/css">
+<!-- Google-code-prettify -->	
+	<link href="themes/js/google-code-prettify/prettify.css" rel="stylesheet"/>
+<!-- fav and touch icons -->
+    <link rel="shortcut icon" href="themes/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="themes/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="themes/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="themes/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="themes/images/ico/apple-touch-icon-57-precomposed.png">
+	<style type="text/css" id="enject"></style>
+  </head>
+<body>
+<div id="header"> 
+<div class="container">
+<div id="welcomeLine" class="row">
+	<div class="span6">Welcome!<strong> User</strong></div>
+	<div class="span6">
+            <div class="pull-right">
+                    <a href="product_summary.html"><span class="">DE</span></a>
+                    <a href="product_summary.html"><span class="">EN</span></a>
+                    <span class="btn btn-mini">HU</span>
+                    <a href="product_summary.html"><span>HUF </span></a>
+                    <span class="btn btn-mini">155.00</span>
+                    <a href="product_summary.html"><span class="">Ft</span></a>
+                    <a href="product_summary.html"><span class="btn btn-mini btn-primary"><i class="icon-shopping-cart icon-white"></i> A kosár üres </span> </a> 
+            </div>
+	</div>
+</div>
+<!-- Navbar ================================================== -->
+<div id="logoArea" class="navbar">
+<a id="smallScreen" data-target="#topMenu" data-toggle="collapse" class="btn btn-navbar">
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+</a>
+  <div class="navbar-inner">
+    <form class="form-inline navbar-search" method="post" action="products.html" >
+		<input id="srchFld" class="srchTxt" type="text" />
+		  <select class="srchTxt">
+			<option>All</option>
+			<option>Akusztikus </option>
+			<option>Elektromos </option>
+			<option>Elektro - Akusztikus </option>
+		</select> 
+		  <button type="submit" id="submitButton" class="btn btn-primary">Keres</button>
+    </form>
+    <ul id="topMenu" class="nav pull-right">
+	 <li class=""><a href="special_offer.html">Specials Offer</a></li>
+	 <li class=""><a href="normal.html">Delivery</a></li>
+	 <li class=""><a href="contact.html">Contact</a></li>
+	 <li class="">
+	 <a href="#login" role="button" data-toggle="modal" style="padding-right:0"><span class="btn btn-large btn-success">Login</span></a>
+	<div id="login" class="modal hide fade in" tabindex="-1" role="dialog" aria-labelledby="login" aria-hidden="false" >
+		  <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+			<h3>Login Block</h3>
+		  </div>
+		  <div class="modal-body">
+			<form class="form-horizontal loginFrm">
+			  <div class="control-group">								
+				<input type="text" id="inputEmail" placeholder="Email">
+			  </div>
+			  <div class="control-group">
+				<input type="password" id="inputPassword" placeholder="Password">
+			  </div>
+			  <div class="control-group">
+				<label class="checkbox">
+				<input type="checkbox"> Remember me
+				</label>
+			  </div>
+			</form>		
+			<button type="submit" class="btn btn-success">Sign in</button>
+			<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+		  </div>
+	</div>
+	</li>
+    </ul>
+  </div>
+</div>
+</div>
+</div>
+<!-- Header End====================================================================== -->
+
+<div id="mainBody">
+	<div class="container">
+	<div class="row">
             
-            
-              <nav>  
-           
-        </nav>
-            
-            
-        <img src="Slashborito.jpg" alt="Slashborito.jpg" title="Slash"/>
-        </div>
-        <footer>
-            &COPY; 2022
-        </footer>
-    </body>
+		<div class="span9">		
+			<!--Jelenleg vásárolható gitárok-->
+			  <ul class="thumbnails">
+				<li class="span3">
+				  <div class="thumbnail">
+					<a  href="product_details.html"><img src="themes/images/products/6.jpg" alt=""/></a>
+					<div class="caption">
+					  <h5>Product name</h5>
+					  <p> 
+						Lorem Ipsum is simply dummy text. 
+					  </p>
+					 
+					  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">$222.00</a></h4>
+					</div>
+				  </div>
+				</li>
+				<li class="span3">
+				  <div class="thumbnail">
+					<a  href="product_details.html"><img src="themes/images/products/7.jpg" alt=""/></a>
+					<div class="caption">
+					  <h5>Product name</h5>
+					  <p> 
+						Lorem Ipsum is simply dummy text. 
+					  </p>
+					 <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">$222.00</a></h4>
+					</div>
+				  </div>
+				</li>
+				<li class="span3">
+				  <div class="thumbnail">
+					<a  href="product_details.html"><img src="themes/images/products/8.jpg" alt=""/></a>
+					<div class="caption">
+					  <h5>Product name</h5>
+					  <p> 
+						Lorem Ipsum is simply dummy text. 
+					  </p>
+					   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">$222.00</a></h4>
+					</div>
+				  </div>
+				</li>
+				<li class="span3">
+				  <div class="thumbnail">
+					<a  href="product_details.html"><img src="themes/images/products/9.jpg" alt=""/></a>
+					<div class="caption">
+					  <h5>Product name</h5>
+					  <p> 
+						Lorem Ipsum is simply dummy text. 
+					  </p>
+					  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">$222.00</a></h4>
+					</div>
+				  </div>
+				</li>
+				<li class="span3">
+				  <div class="thumbnail">
+					<a  href="product_details.html"><img src="themes/images/products/10.jpg" alt=""/></a>
+					<div class="caption">
+					  <h5>Product name</h5>
+					  <p> 
+						Lorem Ipsum is simply dummy text. 
+					  </p>
+					  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">$222.00</a></h4>
+					</div>
+				  </div>
+				</li>
+				<li class="span3">
+				  <div class="thumbnail">
+					<a  href="product_details.html"><img src="themes/images/products/11.jpg" alt=""/></a>
+					<div class="caption">
+					  <h5>Product name</h5>
+					  <p> 
+						Lorem Ipsum is simply dummy text. 
+					  </p>
+					   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">$222.00</a></h4>
+					</div>
+				  </div>
+				</li>
+			  </ul>	
+
+		</div>
+		</div>
+	</div>
+</div>
+<!-- Footer ================================================================== -->
+	<div  id="footerSection">
+	<div class="container">
+		<div class="row">
+			<div class="span3">
+				<h5>ACCOUNT</h5>
+				<a href="login.html">YOUR ACCOUNT</a>
+				<a href="login.html">PERSONAL INFORMATION</a> 
+				<a href="login.html">ADDRESSES</a> 
+				<a href="login.html">DISCOUNT</a>  
+				<a href="login.html">ORDER HISTORY</a>
+			 </div>
+			<div class="span3">
+				<h5>INFORMATION</h5>
+				<a href="contact.html">CONTACT</a>  
+				<a href="register.html">REGISTRATION</a>  
+				<a href="legal_notice.html">LEGAL NOTICE</a>  
+				<a href="tac.html">TERMS AND CONDITIONS</a> 
+				<a href="faq.html">FAQ</a>
+			 </div>
+			<div class="span3">
+				<h5>OUR OFFERS</h5>
+				<a href="#">NEW PRODUCTS</a> 
+				<a href="#">TOP SELLERS</a>  
+				<a href="special_offer.html">SPECIAL OFFERS</a>  
+				<a href="#">MANUFACTURERS</a> 
+				<a href="#">SUPPLIERS</a> 
+			 </div>
+			<div id="socialMedia" class="span3 pull-right">
+				<h5>SOCIAL MEDIA </h5>
+				<a href="#"><img width="60" height="60" src="themes/images/facebook.png" title="facebook" alt="facebook"/></a>
+				<a href="#"><img width="60" height="60" src="themes/images/twitter.png" title="twitter" alt="twitter"/></a>
+				<a href="#"><img width="60" height="60" src="themes/images/youtube.png" title="youtube" alt="youtube"/></a>
+			 </div> 
+		 </div>
+		<p class="pull-right">&copy; Bootshop</p>
+	</div><!-- Container End -->
+	</div>
+<!-- Placed at the end of the document so the pages load faster ============================================= -->
+	<script src="themes/js/jquery.js" type="text/javascript"></script>
+	<script src="themes/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="themes/js/google-code-prettify/prettify.js"></script>
+	
+	<script src="themes/js/bootshop.js"></script>
+    <script src="themes/js/jquery.lightbox-0.5.js"></script>
+	
+
+<span id="themesBtn"></span>
+</body>
 </html>
---
+++ index_old.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
+-->
+<html lang="hu">
+    <head>
+        <title>Gitarshop</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="icon" type="image/png" href="favicon-32x32.png">
+    </head>
+    <body>
+        <header>
+             <h1>Krisztián Ungvári Projektmunkája</h1>
+             <form action="#">
+                 <label>Regisztráció</label>
+                 <input type="password">
+                 <label>Kereső</label>
+                 <input type="search">
+                    <label>Kosár</label>
+                 <input type="">
+             </form>
+        </header>
+       
+        <div id="content">
+            <nav>
+                <ul> 
+                <li> <a href="index.php?menu=fooldal">Főoldal</a> </li> 
+                <li> <a href="index.php?menu=gitarok">Gitárok</a> </li>
+                <li> <a href="index.php?menu=info">Infó</a> </li>
+                <li> <a href="index.php?menu=kapcsolat">Kapcsolat</a> </li>
+                </ul>
+                </nav>
+            <h2>Köszöntelek az oldalamon!</h2>
+     
+       <img src="Slashborito.jpg" alt="Slashborito.jpg" title="Slash"/>
+        </div>
+        <footer>
+            &COPY; 2022 
+        </footer>
+        <footer> KrisztianUngvári:<a href="https://github.com/krisztianungvari"target="_blank">Github</footer>
+    </body>
+</html>
